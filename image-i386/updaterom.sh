8b307013b306cf938370a74ed0a9b2c7a26e28fb
Stephen Soltesz <soltesz@google.com>
2016-09-02 18:29:33 -0400

    run from ipxe/util directory

--- image-i386/updaterom.sh
+++ image-i386/updaterom.sh
@@ -2,6 +2,13 @@
 
 set -x
 set -e
-./util/zbin bin/8086100f.mrom.bin bin/8086100f.mrom.zinfo > bin/8086100f.mrom
-perl ./util/padimg.pl --blksize=512 --byte=0xff bin/8086100f.mrom
-perl ./util/fixrom.pl bin/8086100f.mrom
+
+UTIL=$( dirname "${BASH_SOURCE[0]}" )
+
+MROM_BIN=${1:?mrom bin file}
+MROM_ZINFO=${2:?mrom zinfo file}
+OUTPUT=${3:?output filename}
+
+$UTIL/zbin $MROM_BIN $MROM_ZINFO > $OUTPUT
+perl $UTIL/padimg.pl --blksize=512 --byte=0xff $OUTPUT
+perl $UTIL/fixrom.pl $OUTPUT
