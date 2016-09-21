30fd383957af77aab2278d63d7e7f4737b51ba7a
Stephen Soltesz <soltesz@google.com>
2016-09-20 20:31:03 -0400

    add an update and acknowledge script.

---
+++ image/mlx_config/updaterom.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+# https://epoxy-test-1.measurementlab.net/v1/hosts/$HOSTNAME/ConnectX3.mrom
+
+ROMURL=$( cat /proc/cmdline | tr ' ' '\n' | grep -E 'epoxy.mrom=' | sed -e 's/epoxy.mrom=//g' )
+ACKURL=$( cat /proc/cmdline | tr ' ' '\n' | grep -E 'epoxy.nextboot_disable_url=' | sed -e 's/epoxy.nextboot_disable_url=//g' )
+if test -n "$ROMURL" ; then
+    echo "DOWNLOADING ROM"
+    eval "wget -O epoxy.mrom $ROMURL"
+    if ! test -f epoxy.mrom ; then
+        echo "Error: failed to download epoxy.mrom from $ROMURL"
+        exit 1
+    fi
+    echo "ROM VERSION:"
+    flint --image epoxy.mrom qrom
+
+    echo "UPDATING ROM"
+    /usr/local/util/flashrom.sh epoxy.mrom
+    
+    echo "ACKNOWLEDGE COMPLETE"
+    wget --quiet -O - --post-data='public_ssh_host_key=' $ACKURL > /dev/null
+fi
