f38a6991ec571caa3d71d2c51c93e6a9c8f63f7b
Stephen Soltesz <soltesz@google.com>
2016-09-22 18:32:15 -0400

    check ROM version before continuing, and use long options.

--- image/mlx_config/flashrom.sh
+++ image/mlx_config/flashrom.sh
@@ -6,21 +6,40 @@ set -e
 ROM=${1:?PXE ROM to burn to NIC}
 DEV=/dev/mst/mt4099_pci_cr0
 
+
 # Backup original.
-flint -d "$DEV" rrom backup.mrom
+flint --device "${DEV}" rrom current.mrom
+
+NEW_VERSION=$( flint --image "${ROM}" qrom )
+CUR_VERSION=$( flint --image current.mrom qrom )
+
+DELAY=60
+
+if [[ "${NEW_VERSION}" == "${CUR_VERSION}" ]] ; then
+    echo "Current ROM version matches new ROM version."
+    echo "Sleeping $DELAY seconds..."
+    sleep $DELAY
+    exit 1
+fi
+
+
+echo "ROM Versions:"
+echo "   Currently installed: $CUR_VERSION"
+echo "   Updating to:         $NEW_VERSION"
+
 
 # Query before.
-flint -d "$DEV" q
-mlxconfig -d "$DEV" q
+flint --device "$DEV" query
+mlxconfig --device "$DEV" query
 
 # Burn ROM to NIC.
-flint -d "$DEV" brom "$ROM"
-flint -d "$DEV" verify
+flint --device "$DEV" brom "$ROM"
+flint --device "$DEV" verify
 
 # Query after.
-flint -d "$DEV" q
-mlxconfig -d "$DEV" q
+flint --device "$DEV" query
+mlxconfig --device "$DEV" query
 
 # Extra verify that new ROM matches expected ROM.
-flint -d "$DEV" rrom current.mrom
-diff current.mrom "$ROM"
+flint --device "$DEV" rrom latest.mrom
+diff latest.mrom "$ROM"
